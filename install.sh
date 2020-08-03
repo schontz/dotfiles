ac8588de6f1bcc8cf6617501263a06a6cc0ece87
David Schontzler <2080286+schontz@users.noreply.github.com>
2020-08-03 10:59:01 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,14 +15,19 @@ fi
 # get script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# zsh set up
-mkdir -p ~/.zsh/configs
-mkdir -p ~/.zsh/functions
-cp $DIR/zsh/configs/* ~/.zsh/configs/
-cp $DIR/zsh/functions/* ~/.zsh/functions/
-
-# alias to our dot files
+# zsh
 ln -s $DIR/zshrc ~/.zshrc
 ln -s $DIR/alias ~/.alias
+mkdir -p ~/.zsh/
+ln -s $DIR/zsh/configs ~/.zsh/
+ln -s $DIR/zsh/functions ~/.zsh/
+
+# nvim
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/gvimrc ~/.gvimrc
+mkdir -p ~/.config/nvim
+ln -s $DIR/config/nvim/coc-settings.json ~/.config/nvim/
+ln -s $DIR/config/nvim/cocrc.vim ~/.config/nvim/
+
+# fzf
+ln -s $DIR/fzf.zsh ~/.fzf.zsh
