4289d144af888705792a331ea76328eb6b06b318
David Schontzler <2080286+schontz@users.noreply.github.com>
2020-08-05 11:34:37 -0700

    Updates

--- install.sh
+++ install.sh
@@ -4,27 +4,31 @@ if [ "$(uname)" == "Darwin" ]; then
   echo -e "\\n\\nRunning on macOS"
   
   # Sleep shortcut Cmd-Shift-Option-/
-  defaults write -g NSUserKeyEquivalents -dict-add Sleep "@^$/"
+  # defaults write -g NSUserKeyEquivalents -dict-add Sleep "@^$/"
 
   if test ! "$( command -v brew )"; then
     echo "Installing homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  fi
   
-  brew bundle
+    echo "Installing brew bundle"
+    brew bundle
+  fi
 fi
 
 # get script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # zsh
+echo "Setting up zsh..."
 ln -s $DIR/zshrc ~/.zshrc
 ln -s $DIR/alias ~/.alias
-mkdir -p ~/.zsh/
-ln -s $DIR/zsh/configs ~/.zsh/
-ln -s $DIR/zsh/functions ~/.zsh/
+mkdir -p ~/.zsh/configs
+cp $DIR/zsh/configs/* ~/.zsh/configs/
+mkdir -p ~/.zsh/functions
+cp $DIR/zsh/functions/* ~/.zsh/functions/
 
 # nvim
+echo "Setting up nvim..."
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/gvimrc ~/.gvimrc
 mkdir -p ~/.config/nvim
@@ -33,6 +37,7 @@ ln -s $DIR/config/nvim/cocrc.vim ~/.config/nvim/
 ln -s $DIR/config/nvim/plugin ~/.config/nvim/
 
 # fzf
+echo "Setting up fzf..."
 ln -s $DIR/fzf.zsh ~/.fzf.zsh
 
 # git
