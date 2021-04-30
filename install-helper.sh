5bfb58a356e84c3b6080be79edd88cce1ba3220d
David Schontzler <2080286+schontz@users.noreply.github.com>
2021-04-30 14:49:38 -0700

    Update install-helper.sh

--- install-helper.sh
+++ install-helper.sh
@@ -6,6 +6,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
 if test ! "$( command -v brew )"; then
   # Unattended install
   # https://github.com/Homebrew/legacy-homebrew/issues/46779#issuecomment-162819088
